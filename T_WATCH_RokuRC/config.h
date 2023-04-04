f4bad6f54690f7c19e5401755ac2262b4db1956b
ELSHNKHLL <elshnkhll@yahoo.com>
2023-04-04 19:20:52 -0400

    Add files via upload

---
+++ T_WATCH_RokuRC/config.h
@@ -0,0 +1,9 @@
+#define LILYGO_WATCH_2020_V1       //To use T-Watch2020, please uncomment this line
+
+// Function select
+#define LILYGO_WATCH_LVGL          //To use LVGL, you need to enable the macro LVGL
+#define LILYGO_WATCH_LVGL_FS       //Need to use LVGL file system need to define this macro
+#define LILYGO_WATCH_LVGL_DECODER  //Need to use PNG decoding, to define this macro
+#define LILYGO_WATCH_HAS_MOTOR     //Use Motor module 
+
+#include <LilyGoWatch.h>
